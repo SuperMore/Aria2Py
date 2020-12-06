3af385517b3b90d6e25c3c8990d45db588304dec
愿乘风归去 <moqiqin@live.com>
2020-12-07 01:10:21 +0800

    Create getAria2py.sh

---
+++ getAria2py.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo wget --no-check-certificate https://github.com/SuperMore/Aria2Py/raw/main/aria2_py.py -O /usr/bin/aria2_py.py
+sudo touch /usr/bin/aria2py
+sudo chmod 777 /usr/bin/aria2_py.py
+sudo rm -rf /usr/bin/aria2py
+sudo cat << EOF > /usr/bin/aria2py
+#!/bin/bash
+python3 /usr/bin/aria2_py.py
+EOF
+sudo chmod 777 /usr/bin/aria2py
+echo 'aria2py安装完成，在终端输入 aria2py 即可运行本程序'
